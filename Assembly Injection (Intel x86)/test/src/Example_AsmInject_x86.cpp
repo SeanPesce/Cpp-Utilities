8e64ba7c1a4aa2ea1c71bf26ae0c001f11c512b7
Sean P <pesce.sean@gmail.com>
2017-02-23 11:19:41 -0500

    Make following changes to code in main function to avoid compiler optimizations that would break the program: -Rearrange order of NUMBER-changing arithmetic and print statements -Add dummy conditional based on unreachable value of NUMBER

--- Assembly Injection (Intel x86)/test/src/Example_AsmInject_x86.cpp
+++ Assembly Injection (Intel x86)/test/src/Example_AsmInject_x86.cpp
@@ -13,12 +13,18 @@ int main()
 
     for(int i = 0; i < 10; i++)
     {
-        std::cout << "Value = " << NUMBER << std::endl; // Print NUMBER
 
         // Decrease NUMBER by 2:
         NUMBER -= 2;
+
+        std::cout << "Value = " << NUMBER << std::endl; // Print NUMBER
+
         // Increase NUMBER by 3:
         NUMBER += 3;
+
+        // This conditional will never execute; it's here to stop compiler optimizations that might break the program:
+        if(NUMBER == 1){ std::cout << "Value = " << NUMBER << std::endl; }
+
         // Decrement NUMBER:
         NUMBER--; // Memory location: &main + MAIN_OFFSET
         // Note that NUMBER doesn't change between loop iterations, unless injection is used to remove or alter the above statement(s)
@@ -28,10 +34,10 @@ int main()
         {
             // Unprotect the memory:
             uint32_t oldProtection; // oldProtection is not utilized in Unix (might add this functionality later) @todo
-            SET_MEM_PROTECTION((uint8_t*)&main + MAIN_OFFSET, 10, MEM_PROTECT_RWX, &oldProtection);
+            SET_MEM_PROTECTION((uint8_t*)&main + MAIN_OFFSET, MEM_PROTECT_SIZE, MEM_PROTECT_RWX, &oldProtection);
             
             // Write the jump to the assembly function:
-            injectASM(((uint8_t*)&main + MAIN_OFFSET), &JUMPBACK_ADDRESS, 3, (void*)&asmCodeExample);
+            injectASM(((uint8_t*)&main + MAIN_OFFSET), &JUMPBACK_ADDRESS, NOP_COUNT, (void*)&asmCodeExample);
             
             std::cout << "Injecting assembly at " << (int*)((uint8_t*)&main + MAIN_OFFSET) <<
                         " with return address " << (int*)JUMPBACK_ADDRESS << "..." << std::endl;
