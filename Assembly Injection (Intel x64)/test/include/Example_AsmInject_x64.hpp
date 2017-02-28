813532e4c96ed1dfa58fa592aa27634f61bb0ab7
Sean P <pesce.sean@gmail.com>
2017-02-28 15:57:25 -0500

    Modify Windows and Visual Studio constants/preprocessor values for Windows functionality

--- Assembly Injection (Intel x64)/test/include/Example_AsmInject_x64.hpp
+++ Assembly Injection (Intel x64)/test/include/Example_AsmInject_x64.hpp
@@ -11,27 +11,28 @@
 #include "AsmInject_x64.hpp" // Assembly Injection functions to be demonstrated with this program
 
 int NUMBER = 12; // Will be printed as feedback to show when the injection occurs
-uint64_t JUMPBACK_ADDRESS; // The address used to jump out of the assembly code cave to resume normal program logic
 
 #ifdef _WIN32
     // Define Windows-specific values here
-    #define MEM_PROTECT_SIZE 300
+    #define MEM_PROTECT_SIZE 70
 #else
     // Define Unix-specific values here
-    #define MEM_PROTECT_SIZE 1
+    #define MEM_PROTECT_SIZE 2
 #endif // _WIN32
 
 
 #ifdef _MSC_VER // Using a Microsoft compiler
     // Depending on the compiler, many things can vary:
-    #define MAIN_OFFSET 221     // The injection point
+    #define MAIN_OFFSET 205     // The injection point
     #define NOP_COUNT 45         // The type of instructions used (more importantly here, the number of leftover bytes from those overwritten instructions)
     extern "C" void asmCodeExample();
+    extern "C" uint64_t JUMPBACK_ADDRESS; // The address used to jump out of the assembly code cave to resume normal program logic
 #else
     // Non-Microsoft compiler
     #define MAIN_OFFSET 190
     #define NOP_COUNT 41
     void asmCodeExample();
+    uint64_t JUMPBACK_ADDRESS; // The address used to jump out of the assembly code cave to resume normal program logic
 #endif // _MSC_VER
 
 
